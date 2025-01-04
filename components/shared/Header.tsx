538e249ea32bd6591382beb725d2d2ba39b3279b
kaktus <muniabi@yandex.ru>
2025-01-04 21:43:59 +0300

    add new logo

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -77,31 +77,64 @@ export const Header: React.FC<Props> = ({ className }) => {
                     >
                         <div className="w-8 h-8">
                             <svg
-                                viewBox="0 0 24 24"
+                                viewBox="0 0 32 32"
                                 fill="none"
                                 xmlns="http://www.w3.org/2000/svg"
                                 className="w-full h-full text-[#A559DD]"
                             >
-                                <path
-                                    d="M12 2L2 7L12 12L22 7L12 2Z"
-                                    stroke="currentColor"
-                                    strokeWidth="2"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
+                                {/* Основная вертикальная линия */}
+                                <rect
+                                    x="4"
+                                    y="2"
+                                    width="6"
+                                    height="28"
+                                    fill="currentColor"
                                 />
-                                <path
-                                    d="M2 17L12 22L22 17"
-                                    stroke="currentColor"
-                                    strokeWidth="2"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
+
+                                {/* Верхняя диагональ */}
+                                <rect
+                                    x="10"
+                                    y="12"
+                                    width="6"
+                                    height="6"
+                                    fill="currentColor"
                                 />
-                                <path
-                                    d="M2 12L12 17L22 12"
-                                    stroke="currentColor"
-                                    strokeWidth="2"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
+                                <rect
+                                    x="15"
+                                    y="7"
+                                    width="6"
+                                    height="6"
+                                    fill="currentColor"
+                                />
+                                <rect
+                                    x="20"
+                                    y="2"
+                                    width="6"
+                                    height="6"
+                                    fill="currentColor"
+                                />
+
+                                {/* Нижняя диагональ */}
+                                <rect
+                                    x="10"
+                                    y="14"
+                                    width="6"
+                                    height="6"
+                                    fill="currentColor"
+                                />
+                                <rect
+                                    x="15"
+                                    y="19"
+                                    width="6"
+                                    height="6"
+                                    fill="currentColor"
+                                />
+                                <rect
+                                    x="20"
+                                    y="24"
+                                    width="6"
+                                    height="6"
+                                    fill="currentColor"
                                 />
                             </svg>
                         </div>
