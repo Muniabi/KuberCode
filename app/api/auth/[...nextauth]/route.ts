b249c2f8d29357a0256fd953567e2060fff0ba78
Muniabi <infotkahn@yandex.ru>
2025-04-16 23:00:43 +0300

    test api

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -48,11 +48,11 @@ const authOptions: AuthOptions = {
 
                     console.log("Attempting to authenticate with:", {
                         email: credentials.email,
-                        apiUrl: "http://host.docker.internal:8080",
+                        apiUrl: "http://localhost:8080",
                     });
 
                     const response = await axios.post(
-                        `http://host.docker.internal:8080/api/v1/auth/login`,
+                        `http://localhost:8080/api/v1/auth/login`,
                         {
                             email: credentials.email,
                             password: credentials.password,
