44a089498b1581fbe5e867a4f1b9e85556da5b60
kaktus <muniabi@yandex.ru>
2025-06-20 00:22:16 +0300

    fix ip and nav-user

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -81,6 +81,14 @@ export function NavUser({
                                     {user.name.charAt(0)}
                                 </AvatarFallback>
                             </Avatar>
+                            <div className="grid flex-1 text-left text-sm leading-tight">
+                                <span className="truncate font-semibold">
+                                    {user.name}
+                                </span>
+                                <span className="truncate text-xs">
+                                    {user.email}
+                                </span>
+                            </div>
                             <ChevronsUpDown className="ml-auto size-4" />
                         </SidebarMenuButton>
                     </DropdownMenuTrigger>
